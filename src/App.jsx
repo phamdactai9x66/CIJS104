192e40dc976a97bb1d0345fa372ce8ad4a57dcde
tai.pham <tai15122003311@gmail.com>
2025-06-06 21:15:19 +0700

    implement feture search items

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import Card from "./Components/Card";
 import Header from "./Components/Header";
@@ -7,26 +7,32 @@ import Modal from "./Components/Modal";
 import { statusEnums, tasks } from "./utils/constants";
 
 function App() {
+  const [data, setData] = useState([...tasks]);
+
   return (
     <React.Fragment>
       <div className="App">
-        <Header />
+        <Header setData={setData} />
 
         <div className="container">
           <div className="col">
-            <ListCard tasks={tasks} targetStatus={statusEnums.TODO} />
+            <h1>To Do</h1>
+            <ListCard tasks={data} targetStatus={statusEnums.TODO} />
           </div>
 
           <div className="col">
-            <ListCard tasks={tasks} targetStatus={statusEnums.IN_PROGRESS} />
+            <h1>In Process</h1>
+            <ListCard tasks={data} targetStatus={statusEnums.IN_PROGRESS} />
           </div>
 
           <div className="col">
-            <ListCard tasks={tasks} targetStatus={statusEnums.IN_REVIEW} />
+            <h1>In Preview</h1>
+            <ListCard tasks={data} targetStatus={statusEnums.IN_REVIEW} />
           </div>
 
           <div className="col">
-            <ListCard tasks={tasks} targetStatus={statusEnums.DONE} />
+            <h1>Done</h1>
+            <ListCard tasks={data} targetStatus={statusEnums.DONE} />
           </div>
         </div>
       </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { tasks } from "../utils/constants";
 
-const Header = () => {
+const Header = (props) => {
+  const onSearch = (event) => {
+    const searchTask = tasks.filter((task) => {
+      const searchTitle = task.title.toLowerCase();
+      const searchDescription = task.description.toLowerCase();
+
+      // input value
+      const searchValue = event.target.value.toLowerCase();
+
+      const isTitleMatch = searchTitle.indexOf(searchValue) !== -1;
+
+      const isDescriptionMatch = searchDescription.indexOf(searchValue) !== -1;
+
+      return isTitleMatch || isDescriptionMatch;
+    });
+
+    props.setData(searchTask);
+  };
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <input type="text" name="" id="" placeholder="Searching..." />
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={onSearch}
+        placeholder="Searching..."
+      />
 
       <button>New Items</button>
     </div>
